508adb8da5cf4a25f1658e3a804e6808494f3ccb
Sangeetha G <45sangee@gmail.com>
2024-10-19 21:03:23 +0530

    Requesting squash merge from staging to master branch (#8)
    * Requesting squash merge from my branch to prestaging branch (#1) (#2)
    
    * Implement controller for lead feature
    
    * Implement model for lead feature
    
    * Include lead feature view templates
    
    * Requesting squash merge from prestaging to staging branch (#6)
    
    * Requesting squash merge from my branch to prestaging branch (#1)
    
    * Implement controller for lead feature
    
    * Implement model for lead feature
    
    * Include lead feature view templates
    
    * Requesting squash merge from my branch to prestaging branch (#4)
    
    * Implement controller for contact feature
    
    * Add DB model connectors for lead feature
    
    * Include lead feature view templates
    
    * Feature/dealRequesting squash merge from my branch to prestaging branch   (#5)
    
    * Implement controller for deal feature
    
    * Add DB model connectors for deal feature
    
    * Include deal feature view templates
    
    ---------
    
    Co-authored-by: Aravind M <aravindaravind7139@gmail.com>
    Co-authored-by: Arunkumar  P <arunmech1521@gmail.com>
    
    ---------
    
    Co-authored-by: Aravind M <aravindaravind7139@gmail.com>
    Co-authored-by: Arunkumar  P <arunmech1521@gmail.com>

---
+++ model/contact/__init__.py
@@ -0,0 +1,12 @@
+# Lead feature model implementation.
+ 
+def insert_to_db():
+  print 'Contact record created.'
+ 
+ 
+def update_db():
+  print 'Contact record update.'
+ 
+ 
+def delete_from_db():
+  print 'Contact record deleted.'
